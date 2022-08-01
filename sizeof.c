9d85ea81eb6099e4c8779cd1a2cbe0c59566252c
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-08-01 16:56:23 -0600

    Uploading Template

---
+++ sizeof.c
@@ -0,0 +1,19 @@
+/** sizeof.c
+ * =============================================================
+ * Name: Your Name, Date
+ * Section:  Your Section
+ * Project:  Lab 04
+ * Purpose:  Determine the size of various data types
+ * ============================================================= */
+
+// Create Include Statements Here
+
+
+int main() {
+
+    // Create Your Variables Here
+
+    // Create Your Print Statements Here
+
+    return 0;
+}
